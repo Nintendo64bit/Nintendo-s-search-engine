6466a39c30bcdfb3ff3d466d548fae6316942648
Nintendo64bit <31171515+Nintendo64bit@users.noreply.github.com>
2017-09-15 23:49:40 -0400

    Create install.php

---
+++ install.php
@@ -0,0 +1,26 @@
+ //connect
+             mysql_connect("host","username", "password");
+             mysql_select_db("your choice");
+             
+             $query = mysql_query($query);
+             $numrows = mysql_num_rows($query);
+             if ($numrows > 0) {
+                 while ($row =mysql_fetch_assoc($query)) {
+                     $id = $row['id'];
+                     $title = $row['title'];
+                     $description = $row['description'];
+                     $keywords = $row['keywords'];
+                     $link = $row['link'];
+                     
+                     echo "<h2><a href='$link'>$title</a></h2>
+                     $description<>br />";
+                     
+                    }   
+             }
+             else 
+              echo "No results found for \"<b>$k</b>\""
+             
+             //disconnect
+             mysql_close();
+              
+              connnect with your usernamepassword and host and create the tables
